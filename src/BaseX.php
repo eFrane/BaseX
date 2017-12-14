a295d8ba34eb083edf741a1da5a7ad5f525cd871
Stefan Graupner <efrane@meanderingsoul.com>
2017-12-14 10:21:48 +0100

    Documentation for BaseX

--- src/BaseX.php
+++ src/BaseX.php
@@ -7,8 +7,20 @@
 
 namespace EFrane\BaseX;
 
+/**
+ * BaseX - Quickly convert roman numerals to decimal numbers and back
+ *
+ * @package EFrane\BaseX
+ */
 class BaseX
 {
+    /**
+     * Convert a decimal number to it's roman numeral representation
+     *
+     * @param int $int The decimal number
+     * @param int $base Optional: Anything base_convert() understands
+     * @return string The roman numeral
+     */
     public static function toRoman($int, $base = 10)
     {
         if ($base == 10 && !is_int($int)) {
@@ -22,12 +34,20 @@ class BaseX
         return (new DecimalToRomanConverter())->convert($int);
     }
 
+    /**
+     * Convert a roman numeral to it's decimal number representation
+     *
+     * @param string $string The roman numeral
+     * @param int $base Optional: Anything base_convert() understands
+     * @return string|int The decimal number
+     */
     public static function toDecimal($string, $base = 10)
     {
         if (!is_string($string)) {
             throw new \InvalidArgumentException('Expected roman number literal');
         }
 
-        return base_convert((new RomanToDecimalConverter())->convert($string), 10, $base);
+        $result = (new RomanToDecimalConverter())->convert($string);
+        return ($base == 10) ? $result : base_convert($result, 10, $base);
     }
 }
